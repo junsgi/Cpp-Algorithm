eda21b86da8d6586ef8cf73676737c71b2c1280c
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-06 11:46:49 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 36 ms, Memory: 5040 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,68 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+using namespace std;
+int n, m, graph[1'001][1'001], cost[1'000], visit[1'000], dire[1'000], start, end;
+int MAX = 0x7fffffff / 2, cnt = 1;
+
+void dijkstra()
+{
+	for (int i = 1; i <= n; i++)
+		cost[i] = MAX;
+	cost[start] = 0;
+	int node = start, min = MAX;
+	int tnode = -1;
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (node == -1) return;
+		min = MAX;
+		visit[node] = 1;
+		tnode = -1;
+		for (int j = 1; j <= n; j++)
+		{
+			if (!visit[j] && cost[j] > cost[node] + graph[node][j])
+			{
+				cost[j] = cost[node] + graph[node][j];
+				dire[j] = node;
+			}
+			if (!visit[j] && min > cost[j])
+			{
+				min = cost[j];
+				tnode = j;
+			}
+		}
+		node = tnode;
+	}
+}
+
+void k(int idx, int depth)
+{
+	if (idx == start)
+	{
+		printf("%d\n", depth);
+		printf("%d ", start);
+		return;
+	}
+	k(dire[idx], depth + 1);
+	printf("%d ", idx);
+}
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			if (i != j)graph[i][j] = MAX;
+
+	for (int i = 0; i < m; i++)
+	{
+		int st, ed, c;
+		scanf("%d%d%d", &st, &ed, &c);
+		if (graph[st][ed] > c)
+			graph[st][ed] = c;
+	}
+	scanf("%d%d", &start, & end);
+	dijkstra();
+
+	printf("%d\n", cost[end]);
+	k(end, 1);
+	return 0;
+}
