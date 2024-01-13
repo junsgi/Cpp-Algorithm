12fca1fa74b2162ff0d533521202b6191a3f2cee
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-13 22:25:25 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 1112 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+using namespace std;
+int  M = 1234567891, n;
+char arr[55];
+long long ans, r = 1;
+int main()
+{
+    scanf("%d", &n);
+    scanf("%s", arr);
+    for (int i = 0; i < n; i++)
+    {
+        ans += r * (arr[i] - 96) % M;
+        r *= 31;
+    }
+    printf("%lld", ans);
+    return 0;
+}
