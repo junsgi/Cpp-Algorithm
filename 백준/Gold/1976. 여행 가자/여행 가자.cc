615796dcde92bb76c14f43d3518833d80c73c804
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-17 12:18:42 +0900

    [Gold IV] Title: 여행 가자, Time: 4 ms, Memory: 1896 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,53 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+#include<algorithm>
+#define M 100010
+using namespace std;
+int check[M];
+int n, m, a, ans = 1, MAX;
+int arr[M];
+int find(int node)
+{
+	if (check[node] == node)
+		return check[node];
+	check[node] = find(check[node]);
+	return check[node];
+}
+void Union(int fx, int fy)
+{
+	if (fx < fy)
+		check[fx] = fy;
+	else
+		check[fy] = fx;
+}
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= n; i++) check[i] = i;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			scanf("%d", &a);
+			if (a == 1) // 연결되어 있다면
+			{
+				MAX = MAX < i ? i : MAX; // 연결되어 있는 노드 중 가장 큰 값을 구한다.
+				MAX = MAX < j ? j : MAX;
+
+				int fx = find(i);
+				int fy = find(j);
+				if (fx != fy)
+					Union(fx, fy);
+
+			}
+		}
+	}
+	for (int i = 1; i <= m; i++)
+		scanf("%d", &arr[i]);
+	for (int i = 2; i <= m; i++)
+		if (find(arr[i - 1]) == find(arr[i])) ans++;
+    
+	if (ans == m) printf("YES");
+	else printf("NO");
+	return 0;
+}
