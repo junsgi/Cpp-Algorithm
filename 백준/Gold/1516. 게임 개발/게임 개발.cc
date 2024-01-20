180229553a6d4d7aec9543c84a0f9158e92c42d0
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-20 16:21:41 +0900

    [Gold III] Title: 게임 개발, Time: 8 ms, Memory: 4328 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.cc
@@ -0,0 +1,81 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int n, m, edge[32100], cost[501], ans[501], t;
+int heap[500000], len;
+vector<int> graph[32100];
+int max(int a, int b) { return a < b ? b : a; }
+void up(int idx)
+{
+	if (idx / 2 <= 0) return;
+	if (heap[idx / 2] > heap[idx])
+	{
+		int tmp = heap[idx];
+		heap[idx] = heap[idx / 2];
+		heap[idx / 2] = tmp;
+		up(idx / 2);
+	}
+}
+void down(int idx)
+{
+	int child = idx * 2;
+	if (child > len) return;
+	if (child + 1 <= len && heap[child] > heap[child + 1])
+		child++;
+
+	if (heap[idx] > heap[child])
+	{
+		int tmp = heap[idx];
+		heap[idx] = heap[child];
+		heap[child] = tmp;
+		down(child);
+	}
+}
+int main()
+{
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &m);
+		cost[i] = m;
+		while (1)
+		{
+			scanf("%d", &t);
+			if (t == -1) break;
+			edge[i]++;
+			graph[t].push_back(i);
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (edge[i] == 0)
+		{
+			ans[i] = cost[i];
+			heap[++len] = i;
+			up(len);
+		}
+	}
+
+	while (len)
+	{
+		int node = heap[1];
+		heap[1] = heap[len--];
+		down(1);
+
+		for (int i = 0; i < (int)graph[node].size(); i++)
+		{
+			if (edge[graph[node][i]] == 0) continue;
+			ans[graph[node][i]] = max(ans[graph[node][i]], ans[node] + cost[graph[node][i]]);
+			if (--edge[graph[node][i]] == 0)
+			{
+				heap[++len] = graph[node][i];
+				up(len);
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++)
+		printf("%d\n", ans[i]);
+	return 0;
+}
