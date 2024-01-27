9a3f582301c83b9cf63418d514dfc229189e9041
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-27 12:55:19 +0900

    Update LCA.cc

--- 백준/Gold/11437. LCA/LCA.cc
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -8,13 +8,21 @@ int n, depth[MAX], pa[MAX];
 int max(int a, int b) { return a < b ? b : a; }
 int min(int a, int b) { return a > b ? b : a; }
 vector<int> graph[MAX];
+/*
+LCA
+1. 루트에서부터 모든 노드의 깊이를 구한다.
+2. 두 노드 사이의 깊이를 같게 한다.
+3. 두 노드가 서로 다르다면 각 노드의 부모로 이동한다.
+4. 두 노드가 다르다면 3번 과정을 반복
+5. 같다면 현재 값이 두 노드의 최소 공통 조상
+*/
 void DFS(int node)
 {
 	for (int i = 0; i < (int)graph[node].size(); i++)
 	{
 		if (depth[graph[node][i]] != -1) continue;
-		depth[graph[node][i]] = depth[node] + 1;
-		pa[graph[node][i]] = node;
+		depth[graph[node][i]] = depth[node] + 1; // 깊이 저장
+		pa[graph[node][i]] = node; // 부모 저장
 		DFS(graph[node][i]);
 	}
 }
@@ -37,14 +45,14 @@ int main()
 		int n1, n2;
 		scanf("%d%d", &n1, &n2);
 
-		while (depth[n1] != depth[n2])
+		while (depth[n1] != depth[n2]) // 깊이가 같지 않다면 깊이를 맞춘다.
 		{
 			if (depth[n1] < depth[n2])
 				n2 = pa[n2];
 			else
 				n1 = pa[n1];
 		}
-		while (n1 != n2)
+		while (n1 != n2) // 서로 같지 않다면 조상 노드로 바꾼다.
 		{
 			n1 = pa[n1];
 			n2 = pa[n2];
