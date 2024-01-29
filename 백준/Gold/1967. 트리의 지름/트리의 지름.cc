503bd3746908e8cbeced478a9aa2cfc693d62640
junsgi <104434881+junsgi@users.noreply.github.com>
2024-01-29 14:50:34 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 5276 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,50 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+#include<vector>
+#define M 100'001
+#define MAX(x,y) (((x)>(y)) ? (x):(y))
+using namespace std;
+int n, st, ed, c, visit[M], value[M], ans;
+vector<pair<int, int>> graph[M];
+void DFS(int node)
+{
+	visit[node] = 1;
+	int first = 0, fn = 0, second = 0, sn = 0;
+	for (int i = 0; i < (int)graph[node].size(); i++)
+	{
+		int tnode = graph[node][i].first;
+		int tcost = graph[node][i].second;
+		if (visit[tnode]) continue;
+		DFS(tnode);
+		if (first < value[tnode] + tcost)
+		{
+			if (first)
+			{
+				second = first;
+				sn = fn;
+			}
+			first = value[tnode] + tcost;
+			fn = tnode;
+		}
+		else if (second < value[tnode] + tcost)
+		{
+			second = value[tnode] + tcost;
+			sn = tnode;
+		}
+	}
+	ans = MAX(ans, first + second);
+	value[node] = first;
+}
+int main()
+{
+	scanf("%d", &n);
+	for (int i = 0; i < n - 1; i++)
+	{
+		scanf("%d%d%d", &st, &ed, &c);
+		graph[st].push_back({ ed, c });
+		graph[ed].push_back({ st, c });
+	}
+	DFS(1);
+	printf("%d", ans);
+	return 0;
+}
