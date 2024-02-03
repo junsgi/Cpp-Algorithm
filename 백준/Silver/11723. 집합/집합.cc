6324f8e3b0d249ab032ba434125c3284201f27c4
junsgi <104434881+junsgi@users.noreply.github.com>
2024-02-03 14:37:38 +0900

    [Silver V] Title: 집합, Time: 904 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -10,19 +10,15 @@ int main()
 	{
 		scanf("%s", &com);
 		scanf("%d", &m);
+		int num = m - 1;
 		if (!strcmp(com, "add"))
-			S |= (1 << (m - 1));
+			S |= (1 << num);
 		else if (!strcmp(com, "remove"))
-			S &= ~(1 << (m - 1));
+			S &= ~(1 << num);
 		else if (!strcmp(com, "check"))
-			printf("%d\n", S & (1 << (m - 1)) ? 1 : 0);
+			printf("%d\n", S & (1 << num) ? 1 : 0);
 		else if (!strcmp(com, "toggle"))
-		{
-			if (S & (1 << (m - 1)))
-				S &= ~(1 << (m - 1));
-			else
-				S |= (1 << (m - 1));
-		}
+			S = S & (1 << num) ? S & ~(1 << num) : S | (1 << num);
 		else if (!strcmp(com, "all"))
 			S = (1 << 20) - 1;
 		else
