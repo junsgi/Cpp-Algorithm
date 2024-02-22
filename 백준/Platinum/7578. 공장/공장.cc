30e151494e278d34ee8ddf7910b9b622248099d3
junsgi <104434881+junsgi@users.noreply.github.com>
2024-02-22 14:43:44 +0900

    [Platinum V] Title: 공장, Time: 384 ms, Memory: 9116 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.cc
+++ 백준/Platinum/7578. 공장/공장.cc
@@ -1,8 +1,3 @@
-/*
-0. 두 번째 줄 입력부터 차례대로 줄을 확인해야 하기 때문에 도착지점 이후의 줄 개수만 알면 됨
-1. 두 번째 줄 입력부터 연결되어야 하는 위치를 기준으로 줄의 개수를 기록하여 세그먼트 트리를 만듦
-2. 두 번째 줄 입력의 도착 지점 이후에 줄의 개수를 셈
-*/
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include<algorithm>
@@ -16,7 +11,7 @@ long long ans;
 int insert(int left, int right, int idx, int value)
 {
 	if (value < left || right < value) return seg[idx];
-	if (left == value && right == value)
+	if (left == right)
 		return seg[idx] += 1;
 	int mid = (left + right) / 2;
 	return seg[idx] = insert(left, mid, idx * 2, value) + insert(mid + 1, right, idx * 2 + 1, value);
