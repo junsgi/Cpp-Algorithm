5896f2dfd5d5f2b00c6821a25073465142f15990
junsgi <104434881+junsgi@users.noreply.github.com>
2024-02-24 10:18:35 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 1256 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,42 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+struct temp {
+	long long st, ed, cost;
+}arr[6000];
+int n, m, a, b, c;
+long long visit[501], MAX = 1'000'000'000;
+int BellmanFord()
+{
+	int res = 0;
+	for (int i = 0; i < n; i++)
+	{
+		res = 1;
+		for (int j = 0; j < m; j++)
+		{
+			if (visit[arr[j].st] == MAX) continue;
+			if (visit[arr[j].ed] > visit[arr[j].st] + arr[j].cost)
+			{
+				visit[arr[j].ed] = visit[arr[j].st] + arr[j].cost;
+				res = 0;
+			}
+		}
+
+	}
+	return res;
+}
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for (int i = 2; i <= n; i++) visit[i] = MAX;
+	for (int i = 0; i < m; i++)
+		scanf("%lld%lld%lld", &arr[i].st, &arr[i].ed, &arr[i].cost);
+	if (BellmanFord())
+	{
+		for (int i = 2; i <= n; i++)
+			printf("%lld\n", visit[i] == MAX ? -1 : visit[i]);
+	}
+	else printf("-1");
+	return 0;
+}
