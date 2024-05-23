1c59b0da871bc982dc7385f57a3a83c40a86b0ed
junsgi <104434881+junsgi@users.noreply.github.com>
2024-05-24 01:42:54 +0900

    [level 3] Title: 풍선 터트리기, Time: 305.59 ms, Memory: 50.1 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -44,38 +44,3 @@ int solution(vector<int> a)
         
     return answer;
 }
-
-/*
-def solution(a):
-    answer = 0
-    LEN = len(a)
-    if LEN <= 2: return LEN
-    MAX = 0x7fffffff
-    cnt = 1
-    c = 1
-    while cnt <= LEN: cnt *= 2; c += 1
-    seg = [MAX] * (1 << c)
-    insert(seg, a, cnt)
-    answer = 2
-    for i in range(1, LEN - 1):
-        if query(1, cnt, 1, 1, i, seg) > a[i] or query(1, cnt, 1, i + 2, LEN, seg) > a[i]:
-            answer += 1
-    return answer
-def insert(seg, a, cnt):
-    for i in range(len(a)):
-        idx = cnt + i
-        seg[idx] = a[i]
-        idx //= 2
-        while idx > 0:
-            seg[idx] = min(seg[idx * 2], seg[idx * 2 + 1])
-            idx //= 2
-def query(left, right, idx, st, ed, seg):
-    if ed < left or right < st: 
-        return 0x7fffffff
-    if st <= left and right <= ed: 
-        return seg[idx]
-    mid = (left + right) // 2
-    return min(query(left, mid, idx * 2, st, ed, seg), query(mid + 1, right, idx * 2 + 1, st, ed, seg))
-
-
-*/
